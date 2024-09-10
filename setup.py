bcc7597e63b70b47e447604f263ce147fc3de3f4
Bart Thate <rssbotd@gmail.com>
2024-09-10 23:56:30 +0200

    up version

--- setup.py
+++ setup.py
@@ -8,4 +8,4 @@ import setuptools
 
 
 if __name__ == "__main__":
-    setuptools.setup(scripts=["bin/objbot"])
+    setuptools.setup(scripts=["bin/objbot", "bin/objbotc" , "bin/objbotd", "bin/objbots"])
