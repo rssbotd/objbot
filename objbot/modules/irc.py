495e95d672155f94008de86941fc265a67bffcac
Bart Thate <rssbotd@gmail.com>
2024-09-12 22:53:01 +0200

    add irc to broker

--- objbot/modules/irc.py
+++ objbot/modules/irc.py
@@ -178,6 +178,7 @@ class IRC(Client, Output):
     def __init__(self):
         Client.__init__(self)
         Output.__init__(self)
+        Broker.register(self)
         self.buffer = []
         self.cfg = Config()
         self.channels = []
