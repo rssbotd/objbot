ccd9406d7fa0d3432b5f25d86796c535518d9fb7
Bart Thate <rssbotd@gmail.com>
2024-09-12 18:36:59 +0200

    add face

---
+++ objbot/face.py
@@ -0,0 +1,70 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0401,W0611,W0614,W0622
+# ruff: noqa: F401,F403
+
+
+"interface"
+
+
+from . import broker, command, main, object, reactor, thread, workdir
+
+
+from .broker  import *
+from .command import *
+from .main    import *
+from .object  import *
+from .reactor import *
+from .thread  import *
+from .workdir import * 
+
+
+def __dir__():
+    return (
+        'Broker',
+        'CLI',
+        'Commands',
+        'Config',
+        'config',
+        'Console',
+        'Default',
+        'Errors',
+        'Event',
+        'Handler',
+        'Logging',
+        'Object',
+        'Reactor',
+        'Repeater',
+        'Thread',
+        'Timer',
+        'Workdir',
+        'boot',
+        'broker',
+        'banner',
+        'command',
+        'daemon',
+        'debug',
+        'errors',
+        'event',
+        'fetch',
+        'find',
+        'fns',
+        'fntime',
+        'getmods',
+        'laps',
+        'last',
+        'later',
+        'launch',
+        'long',
+        'modnames',
+        'named',
+        'privileges',
+        'read',
+        'scan',
+        'skel',
+        'spl',
+        'store',
+        'strip',
+        'sync',
+        'wrap',
+        'write'
+    )
